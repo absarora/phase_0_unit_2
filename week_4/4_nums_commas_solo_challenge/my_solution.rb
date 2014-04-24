4f12521b6f1062cc174014b55a0e53cd82b76e98
absarora <absingharora@gmail.com>
2014-04-23 22:06:59 -0700

    Update my_solution.rb

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -12,18 +12,66 @@
 
 # What is the output? (i.e. What should the code return?)
 # separate_comma(0) = "0"
-# separate_comma(0) = "0"
-# separate_comma(0) = "0"
+# separate_comma(100) = "100"
+# separate_comma(1000) = "1,000"
+# separate_comma(1000000) = "1,000,000"
+
 # What are the steps needed to solve the problem?
 
+# 1. convert fixnum to a string
+# 2. convert string to an array: separate every digit
+# 3. if the length of an array < 4, return array as a string
+# 4. if the length of an array == 4, add a comma at index 1
+# 5. else add a comma at index 3 (increment index by 4)
 
 # 2. Initial Solution
 
+# The solution below would work if the output is as listed:
+# separate_comma(1000000) => "1,000,000"
+# This is similar to the example given in the exercise
 
+def separate_comma(int)
+  array = int.to_s.split("")
+  new_array=[]
+  if array.length < 4
+    x = array
+  end
+  index = 1
+  while index < array.length
+    if array.length >= 4
+      x = array.insert(index, ",")
+    end
+    index +=4
+  end
+  (new_array << x).join("")
+end
 
 
 # 3. Refactored Solution
 
+# The solution below is different from the one written above
+# This solution passes the tests written in the spec file
 
+def separate_comma(int)
+  array = int.to_s.split("")
+  new_array=[]
+  if array.length < 4
+    x = array
+  end
+  index = 3
+  while index < array.length
+    if array.length == 4
+        x = array.insert(1, ",")
+    elsif array.length > 4
+      x = array.insert(index, ",")
+    end
+    index +=4
+  end
+  (new_array << x).join("")
+end
 
-# 4. Reflection 
+# 4. Reflection
+
+# Writing the pseudocode really helped get to the solution, quick. As explained in the exercise,
+# I was able to get the desired output only to know that a different logic is required to pass
+# the tests. With slight modifications, I was able to get to the solution.
