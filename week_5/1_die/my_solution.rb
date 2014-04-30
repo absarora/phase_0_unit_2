6ab933bb7f0c350ecce9d87a6974a59fe8da4244
absarora <absingharora@gmail.com>
2014-04-30 01:14:46 -0700

    Update my_solution.rb

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,28 +1,33 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Die.new(6) 
+# Output: # of sides and a random # between 1 and 6 
 # Steps:
+# raise an argument error if sides < 1
+# create an instance variable
+# return the # of sides the user input
+# return a random # between 1 and 6
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+    raise ArgumentError.new("Number must be greater than or equal to 1") if sides < 1
+    @sides = sides
   end
   
   def sides
-    # code goes here
+    @sides
   end
   
   def roll
-    # code goes here
+    rand(1..@sides)
   end
 end
 
@@ -38,8 +43,11 @@ end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 
+die = Die.new(6)
 
 
 
+# 5. Reflection
 
-# 5. Reflection 
+# There was a lot of learning involved for this exercise. I am not very comfortable with using classes, yet. I didn't
+# know how to raise an argument error but this link (http://apidock.com/ruby/ArgumentError) helped me get past that.
