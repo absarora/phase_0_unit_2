d8b56d9d22f6cafe60f62f6694ecd7eacc0c08b8
absarora <absingharora@gmail.com>
2014-04-29 18:32:01 -0700

    Update my_solution.rb

--- week_4/6_cipher_challenge/my_solution.rb
+++ week_4/6_cipher_challenge/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W4: Cipher Challenge
 
 
-# I worked on this challenge with: .
+# I worked on this challenge with: Andy Principe.
 
 
 
@@ -73,18 +73,60 @@ end
 
 # Your Refactored Solution
 
+def north_korean_cipher(coded_message)
+  input = coded_message.downcase.split("") # puts the string to all lowercase and converts the string to an array
+  decoded_sentence = []   # Creates an empty array
+  cipher = [] 
+  alphabets = ("a".."z").to_a
+  alphabets.each_index{|i| cipher[i] = alphabets[i+4]}
+  # cipher[0] == alphabets[4]
+  cipher.compact!.push("a","b","c","d") #compact gets rid of all the nil elements from an array
+  input.each do |x| #each is iterating over an array, named input
+    found_match = false # no match found yet
+    cipher.each_index do |y| # iterate over the indexes of an array
+      if x == cipher[y] # comparing elements of input array to elements of cipher array
+        puts "I am comparing x and y. X is #{x} and Y is #{cipher[y]}."
+        decoded_sentence << cipher[y-4]
+        found_match = true
+        break  # if match found, end the loop
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #checking for special characters
+        decoded_sentence << " "
+        found_match = true
+        break
+      elsif (0..9).to_a.include?(x)
+        decoded_sentence << x
+        found_match = true
+        break
+      end 
+    end
+    if not found_match  # if no match found, return the original array
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+  # "I want a coke!"
+ 
+  if decoded_sentence.match(/\d+/) # if the decoded sentence include digits [0-9] 
 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 }
+    # convert the digits to an integer and divide it by 100
+  end  
+  return decoded_sentence # returns the final string (decoded sentence)        
+end
 
 
 
 # Driver Code:
 p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
 # Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
-p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.")
-p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!")
-p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")
-p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
-p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") == "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
 
 # Reflection
+
+# I learned about methods like "compact" and "gsub" to complete this exercise but overall, it wasn't that bad. The code
+# works.
  
