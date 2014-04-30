aa684a81f9d53e8377899694230dc88bfa67c6ac
absarora <absingharora@gmail.com>
2014-04-30 04:34:47 -0700

    Update my_solution.rb

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -5,23 +5,41 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: GuessingGame.new(10)
+# Output: 
+#---------------------------
+# game.solved?   # => false
 
+# game.guess(5)  # => :low
+# game.guess(20) # => :high
+# game.solved?   # => false
+
+# game.guess(10) # => :correct
+# game.solved?   # => true
+#---------------------------
+# Steps:
+# set instance variables for initialize and guess
+# return high if guess is too high
+# return correct if guess is equal to the input
+# return low if guess is too low
+# if guess equal the input, return true for we have solved the guessing game
 
 # 3. Initial Solution
 
 class GuessingGame
   def initialize(answer)
-    # Your initialization code goes here
+    @answer = answer
   end
-  
-  # Make sure you define the other required methods, too
+  def guess(guess)
+    @guess = guess
+    return :high if guess > @answer
+    return :correct if guess == @answer
+    return :low if guess < @answer
+  end
+  def solved?
+    @guess == @answer
+  end
 end
-
-
-
 
 # 4. Refactored Solution
 
@@ -32,9 +50,20 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+game = GuessingGame.new(10)
 
+game.solved?   # => false
 
+game.guess(5)  # => :low
+game.guess(20) # => :high
+game.solved?   # => false
 
+game.guess(10) # => :correct
+game.solved?   # => true
 
 
 # 5. Reflection 
+
+# This is one of the exercises provided in the array of exercises given to us before week 0 and I did finish it
+# back then. The user story provided for this is straight forward and easy to follow through to finish this
+# exercise. I didn't have any trouble for this exercise.
