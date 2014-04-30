1c0f33ab4ec60947a992e50d287f0c823051f962
absarora <absingharora@gmail.com>
2014-04-30 02:25:07 -0700

    Update my_solution.rb

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -1,25 +1,33 @@
 # U2.W5: Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Die.new(array)
+# Output: length of an array and a random element from an array
 # Steps:
+# raise an argument if labels is an empty array
+# set an instance variable in the initialize method
+# return the length of the labels array
+# return a random element from an array
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(labels)
+    raise ArgumentError.new("List of labels is empty") if labels == []
+    @labels = labels
   end
 
   def sides
+    @labels.length
   end
 
   def roll
+    @labels.sample
   end
 end
 
@@ -35,8 +43,11 @@ end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 
+die = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
 
 
 
+# 5. Reflection
 
-# 5. Reflection 
+# This was comparatively an easier exercise to work on compared to the last one. I understand argument error better
+# now.
