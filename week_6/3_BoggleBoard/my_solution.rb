356d1e52bd0ac858a82b8e96ed02fa8ec0c8e919
absarora <absingharora@gmail.com>
2014-05-08 07:00:21 -0700

    Update my_solution.rb

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -1,18 +1,33 @@
 # U2.W6: Create a BoggleBoard Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Pseudocode
 
-
+# create the class constructor
+# declare instance variable
+# use instance variable within the methods of an object
+# test the code
 
 # 3. Initial Solution
 class BoggleBoard
+  def initialize(board)
+    @board = board
+  end
  
-  #your code here
- 
+  def create_word(*coords)
+    coords.map { |coord| @board[coord.first][coord.last]}.join("")
+  end
+  
+  def get_row(row)
+    @board[row]
+  end
+
+  def get_col(col)
+    @board.transpose[col]
+  end
 end
  
  
@@ -34,7 +49,21 @@ boggle_board = BoggleBoard.new(dice_grid)
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
+
+puts boggle_board.create_word([1,2],[1,1],[2,1],[3,2]) == "dock"
+
 # implement tests for each of the methods here:
 
+num = 0
+while num < 4
+print "row #{num+1}: "
+p boggle_board.get_row(num)
+
+print "col #{num+1}: "
+p boggle_board.get_col(num)
+num += 1
+end
 
 # 5. Reflection 
+
+# I feel more comfortable using classes now; this was good practice.
