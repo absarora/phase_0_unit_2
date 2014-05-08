d95ae4b696d8b3ecf6a13a9ea3c357d25ad06874
absarora <absingharora@gmail.com>
2014-05-08 11:44:47 -0700

    Update my_solution.rb

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,17 +1,60 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Pseudocode
 
+# Create a class named "Song"
+# => create a constructor
+# => create an instance method "play" that outputs a string
+
+# Create a class named "Playlist"
+# => create a constructor
+# => create an instance method "add" that add tracks to the playlist
+# => create an instance method "num_of"tracks" that returns the total # of tracks in the playlist
+# => create an instance method "play_all" that plays the playlist
+# => create an instance method "remove" that removes a track
+# => create an instance method "display" that displays all the tracks
+# => create an instance method "includes?" that checks to see if the specific track is available
 
 
 # Initial Solution
 
+class Song
+  def initialize(track, artist)
+    @track = track
+    @artist = artist
+  end
+  def play
+    puts "#{@track} by #{@artist}"
+  end
+end
 
-
+class Playlist
+  def initialize(*playlist)
+    @playlist = playlist
+  end
+  def add(*tracks)
+    tracks.each{|track| @playlist << track}
+  end
+  def num_of_tracks
+    @playlist.count
+  end
+  def play_all
+    @playlist.each(&:play)
+  end
+  def remove(track)
+    @playlist.delete(track)
+  end
+  def display
+    puts @playlist.inspect
+  end
+  def includes?(track)
+    @playlist.include?(track)
+  end
+end
 
 # Refactored Solution
 
@@ -39,7 +82,7 @@ my_playlist.play_all
 my_playlist.display
 
 
+# Reflection
 
-
-
-# Reflection 
+# Everything seems to work but I doubt I have the "display" method setup correctly
+# to display the tracks and artists.
